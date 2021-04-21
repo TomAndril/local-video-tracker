c88d2affd03f435126397ce9a382f2fe6ba01dd6
Tomas Nasjleti <acatomas@gmail.com>
2021-04-21 02:00:18 +0200

    removed unecessary user slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import projectsSlice from './slices/projectsSlice';
 import UISlice from './slices/UISlice';
-import userSlice from './slices/userSlice';
 
 const store = configureStore({
   reducer: {
-    user: userSlice,
     projects: projectsSlice,
     UI: UISlice,
   },
